ef28c8895deac495776b2c3976e40dce195ffb78
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-12-02 13:12:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
-import React, {Component, useState} from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
-  const handleClick=()=>{
+
+  const handleClick = () => {
     setIsVisible(true);
-  }
+  };
+
   return (
     <div id="main">
-    <button onClick={handleClick}>click me</button>
-  {isVisble ? (
-    <p id="para">
+      <button onClick={handleClick} id="click">
+        Click Me
+      </button>
+
+      {isVisible && (
+        <p id="para">
           Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
         </p>
-    ) : (
-      ""
-    )}
+      )}
     </div>
   );
 }
-
 
 export default App;
